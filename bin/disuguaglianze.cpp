71651c00e73aaae26dc7a236578706f93e447ba4
DavideC03 <davide.castellani.5.8.03@gmail.com>
2021-05-12 22:33:44 +0200

    Auto-update

--- bin/disuguaglianze.cpp
+++ bin/disuguaglianze.cpp
@@ -3,7 +3,7 @@
  *
  * @version 01.01 202158
  *
- * @brief 
+ * @brief
  *
  * @ingroup disuguaglianze
  * (Note: this needs exactly one @defgroup somewhere)
@@ -20,23 +20,55 @@ using namespace std;
 
 // Variabiles
 int N;
+vector <char> versi;
+vector <int> sol;
 
 // Main code
 int main()
 {
-  // Cncomment the following lines if you want to read/write from files
-  // freopen("input.txt", "r", stdin);
-  // freopen("output.txt", "w", stdout);
+    // Cncomment the following lines if you want to read/write from files
+    // freopen("input.txt", "r", stdin);
+    // freopen("output.txt", "w", stdout);
 
-  // Input
-  cin >> N;
+    // Input
+    cin >> N;
+    versi.resize(N-1);
+    sol.resize(N+1);
 
-  // Code
-  // ...
+    for(size_t i = 0; i < N - 1; ++i)
+    {
+        cin >> versi[i];
+    }
 
-  // Output
-  cout << N << endl;
+    for(size_t i = 0; i < N+1; ++i)
+    {
+        sol[i] = i + 1;
+    }
 
-  // End
-  return 0;
+    size_t i = 0;
+    while(i < N)
+    {
+        int s = i;
+        while (versi[i] == '<')
+            ++i;
+
+        if (s != i)  //sol.size() - 1
+        {
+            int tmp = sol[s];
+            sol[s] = sol[i+1];
+            sol[i+1] = tmp;
+            i -= 2;
+            if (i < 0)
+                i = -1;
+        }
+        ++i;
+    }
+
+    for(size_t i = 0; i < N; ++i)
+    {
+        cout << sol[i] << " ";
+    }
+
+    // End
+    return 0;
 }
